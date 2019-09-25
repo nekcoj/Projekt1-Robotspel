0ae77036f2797e18b2e4f79d3950acb4cb539d82
nekcoj <55090544+nekcoj@users.noreply.github.com>
2019-09-25 15:23:31 +0200

    skapat metod antRobotar

--- src/com/company/Spel.java
+++ src/com/company/Spel.java
@@ -1,4 +1,15 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Spel {
+
+    private Robot[] rVekt;
+
+    public void antRobotar(){
+        System.out.println("Hur många robotar vill du skapa?");
+        Scanner scan = new Scanner(System.in);
+        int antRobotar = scan.nextInt();
+        rVekt = new Robot[antRobotar];
+    }
 }
