3fca0d89a5004d1c08291a4e8853034bfc266793
nekcoj <55090544+nekcoj@users.noreply.github.com>
2019-09-25 15:30:18 +0200

    lagt in kod i main som skapar ett spel och testar metoden antRobotar

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Spel s = new Spel();    //skapar nytt spel
+	s.antRobotar();         //anropar metoden där användaren får ange hur många robotar som ska skapas
     }
 }
