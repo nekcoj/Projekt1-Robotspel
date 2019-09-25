742fb44be1abeb5a5971c39d4b6e570c2ff6b697
AndyHua5757 <aphua5757@gmail.com>
2019-09-25 15:41:21 +0200

    koda i punkt.java

--- src/com/company/Punkt.java
+++ src/com/company/Punkt.java
@@ -1,4 +1,36 @@
 package com.company;
 
 public class Punkt {
+    private int x, y;
+
+    Punkt(){
+        x = 0;
+        y = 0;
+    }
+
+    public void setX(int x){
+        if (x>0){
+            this.x = x;
+        }
+        else{
+            x = 0;
+        }
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public void setY(int y){
+        if (y>0){
+            this.y = y;
+        }
+        else{
+            y = 0;
+        }
+    }
+
+    public int getY(){
+        return y;
+    }
 }
